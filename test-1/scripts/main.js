529eb4034caa83b2f936ab761fd418dafdfe5794
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-03-27 01:02:53 +0000

    ~init project

---
+++ test-1/scripts/main.js
@@ -0,0 +1,36 @@
+$(window).on('load', function () {
+	$.getJSON('./storage/data.json', function (data) {
+		({ earnings, consultant } = data)
+		console.log(earnings);
+		setEarnings(earnings)
+		setConsultant(consultant)
+	});
+})
+
+function setEarnings(data) {
+	var formatted = formatNumber(data['earnings']['amount'])
+	setValueToElement('#earnings', formatted)
+	setValueToElement('#currency', data['earnings']['currency'])
+	formatted = formatNumber(data['paid-calls'])
+	setValueToElement('#calls', formatted)
+}
+
+function setConsultant(data) {
+	const consultants = data.sort((a, b) => b['revenue'] - a['revenue'])
+	var innerHTML = ''
+	consultants.forEach((consultant) => {
+		innerHTML += `<div class="consultant-lader">
+		<div class="consultant-revenue">${formatNumber(consultant['revenue'])}</div>
+		<div class="consultant-name">${consultant['name']}</div>
+		</div>`
+	})
+	$('#consultants-stats').html(innerHTML)
+}
+
+function setValueToElement(elemID, value) {
+	$(elemID).text(value);
+}
+
+function formatNumber(numStr) {
+	return parseFloat(numStr).toLocaleString('en')
+}
